1fe64dc586d9f123c18bdea53290a6f6f6cdaeaa
Juan Jose Duarte Urrego <cuartoa2016juanjose@gmail.com>
2026-03-02 16:31:13 -0500

    creacion de recursos

--- TareaPrincipal/ProyectoBackend/src/App.java
+++ TareaPrincipal/ProyectoBackend/src/App.java
@@ -84,7 +84,8 @@ public class App {
          * usamos opciones para el menu principal, y opcion1, opcion2, opcion3, opcion4,
          * opcion5, opcion6 para los submenus de cada modelo.
          */
-        int opciones = 0, opcion1 = 0, opcion2 = 0, opcion3 = 0, opcion4 = 0, opcion5 = 0, opcion6 = 0;
+        int opciones = 0, opcion1 = 0, opcion2 = 0, opcionrecursos = 0, opcionproyectos = 0, opcionparticipantes = 0,
+                opcionpatrocinio = 0, opcionmantenimiento = 0;
 
         // MENU PRINCIPAL
         do {
@@ -94,7 +95,6 @@ public class App {
                     + "3. salir"));
             switch (opciones) {
                 case 1:
-
                     // SUBMENU DE INICIO DE SESION, para que el usuario quiera o no iniciar sesion
                     do {
                         opcion1 = Integer.parseInt(JOptionPane.showInputDialog("BIENVENIDO INICIO DE SESION \n"
@@ -142,7 +142,190 @@ public class App {
                                                 break;
                                             case 3:
                                                 // aqui iria el submenu de recursos
+                                                do {
+                                                    opcionrecursos = Integer
+                                                            .parseInt(JOptionPane.showInputDialog("RECURSOS \n"
+                                                                    + "1. Ver todos los recursos \n"
+                                                                    + "2. Registrar algun recurso  \n"
+                                                                    + "3. Actualizar recurso \n"
+                                                                    + "4. Eliminar recurso \n"
+                                                                    + "5. salir"));
+                                                    switch (opcionrecursos) {
+
+                                                        case 1:
+                                                            // ver todos los recursos
+                                                            if (recursosList.isEmpty()) {
+                                                                JOptionPane.showMessageDialog(null,
+                                                                        "No hay recursos registrados");
+                                                                // lo de arriba, el is.empty, es para saber si el
+                                                                // arrayList de recursos esta vacia
+                                                                // si es asi entonces tira el mensaje "no hay recursos".
+                                                            } else {
+                                                                String hojadepapel = " "; // esto tendria que ir en
+                                                                                          // recursos
+                                                                // esto es un contador es para que no se puedan repitir
+                                                                // datos, si lo ponemos arriba al principio se muestran
+                                                                // los datos
+                                                                for (recursos mostrarlista : recursosList) {
+                                                                    // bucle for each que es lo mejor para mostrar lo
+                                                                    // que tiene un arreglo, en este caso
+                                                                    // el ArrayList
+                                                                    // recursos: es el modelo que creamos
+                                                                    // mostrarlista: nombre que le damos al for each
+                                                                    // para mostrar los datos, se puede poner cualquier
+                                                                    // nombre
+                                                                    // recursosList: es el ArrayList donde se guardan
+                                                                    // los objetos de recursos
+                                                                    //
+                                                                    hojadepapel += "ID: " + mostrarlista.getRecursoId()
+                                                                            + "\n"
+                                                                            + "nombre: "
+                                                                            + mostrarlista.getNombre_delrecurso() + "\n"
+                                                                            + "categoria: "
+                                                                            + mostrarlista.getCategoria_delrecurso()
+                                                                            + "\n"
+                                                                            + "estado: "
+                                                                            + mostrarlista.getEstado_delrecurso() + "\n"
+                                                                            + "\n"
+                                                                            + "ubicacion:"
+                                                                            + mostrarlista.getUbicacion_delrecurso();
+                                                                }
+                                                                // mostrar lista
+                                                                JOptionPane.showMessageDialog(null,
+                                                                        "LISTA DE RECURSOS \n"
+                                                                                + hojadepapel);
+                                                            }
+                                                            break;
+
+                                                        case 2:
+                                                            // registrar algun recurso
+                                                            Id_delrecurso = JOptionPane
+                                                                    .showInputDialog("Ingrese el ID del recurso:");
+                                                            // pedimos un ID para el recurso
+                                                            boolean IDyaexiste = false; // apagado
+                                                            // creamos una variable booleana para saber si el ID
+                                                            // ingresado ya existe
+                                                            for (recursos comprobarIDrecurso : recursosList) {
+                                                                // bucle for each para buscar el ID ingresado en el
+                                                                // ArrayList de recursos
+                                                                if (comprobarIDrecurso.getRecursoId()
+                                                                        .equals(Id_delrecurso)) {
+                                                                    IDyaexiste = true; // si lo encontramos, entonces
+                                                                                       // esto se enciende
+                                                                    // si lo encontrmamos, el for para.
+                                                                    break;
+                                                                }
+                                                            }
+                                                            if (IDyaexiste) {
+                                                                JOptionPane.showMessageDialog(null, "ese ID ya existe");
+                                                            } else {
+                                                                nombre_delrecurso = JOptionPane.showInputDialog(
+                                                                        "Ingrese el nombre del recurso:");
+                                                                categoria_delrecurso = JOptionPane.showInputDialog(
+                                                                        "Ingrese la categoria del recurso:");
+                                                                estado_delrecurso = JOptionPane.showInputDialog(
+                                                                        "Ingrese el estado del recurso:");
+                                                                ubicacion_delrecurso = JOptionPane.showInputDialog(
+                                                                        "Ingrese la ubicacion del recurso:");
+                                                                // pedimos los datos para el modelo recurso
+                                                                recursosList.add(new recursos(Id_delrecurso,
+                                                                        nombre_delrecurso, categoria_delrecurso,
+                                                                        estado_delrecurso, ubicacion_delrecurso));
+                                                                // lo pasamos por el constructor del modelo y lo
+                                                                // guardamos en el ArrayList de recursos
+                                                                JOptionPane.showMessageDialog(null,
+                                                                        "Recurso registrado exitosamente");
+                                                                // mensaje de recurso registrado exitosamente
+                                                            }
+                                                            break;
+
+                                                        case 3:
+                                                            // lo mismo pero para actualizar un recurso
+                                                            Id_delrecurso = JOptionPane.showInputDialog(
+                                                                    "Ingrese el ID del recurso que desea actualizar:");
+                                                            // pedimos el ID
+                                                            boolean IDencontrado = false; // apagado
+                                                            for (recursos actualizar : recursosList) {
+                                                                if (actualizar.getRecursoId().equals(Id_delrecurso)) {
+                                                                    IDencontrado = true; // si encontramos el ID,
+                                                                                         // entonces esto se enciende
+
+                                                                    // pedimos los nuevos datos
+                                                                    nombre_delrecurso = JOptionPane.showInputDialog(
+                                                                            "Ingrese el nuevo nombre del recurso:");
+                                                                    categoria_delrecurso = JOptionPane.showInputDialog(
+                                                                            "Ingrese la nueva categoria del recurso:");
+                                                                    estado_delrecurso = JOptionPane.showInputDialog(
+                                                                            "Ingrese el nuevo estado del recurso:");
+                                                                    ubicacion_delrecurso = JOptionPane.showInputDialog(
+                                                                            "Ingrese la nueva ubicacion del recurso:");
+
+                                                                    // actualizamos el modelo con los nuevos datos
+                                                                    actualizar.setNombre_delrecurso(nombre_delrecurso);
+                                                                    actualizar.setCategoria_delrecurso(
+                                                                            categoria_delrecurso);
+                                                                    actualizar.setEstado_delrecurso(estado_delrecurso);
+                                                                    actualizar.setUbicacion_delrecurso(
+                                                                            ubicacion_delrecurso);
+                                                                    JOptionPane.showMessageDialog(null,
+                                                                            "Recurso actualizado exitosamente");
+                                                                    break;
+                                                                }
+                                                            }
+                                                            // como se quedo apagado el IDencontrado, entonces le
+                                                            // decimos que no se encontro.
+                                                            if (!IDencontrado) {
+                                                                JOptionPane.showMessageDialog(null, "ID no encontrado");
+                                                            }
+                                                            break;
+
+                                                            //---------------------------------------------------------------------
+                                                        case 4:
+                                                            // En este caso nos toca buscar el ID
+                                                            Id_delrecurso = JOptionPane.showInputDialog(
+                                                                    "Ingrese el ID del recurso que desea eliminar:");
+                                                            boolean encontradoparaelminar = false; // false, osea no lo
+                                                                                                   // ha encontrado
+                                                                                                   // entonces permanece
+                                                                                                   // apagado
+                                                            char confirmacion;
+                                                            do{
+                                                            confirmacion = JOptionPane.showInputDialog(
+                                                                    "¿Está seguro que desea eliminar este recurso? S o N ")
+                                                                    .toUpperCase().charAt(0);
+                                                                        if(confirmacion == 'S' || confirmacion == 's'){
+                                                                        for (int i = 0; i < recursosList.size(); i++) {
+                                                                // recorre desde la posicion 0 hasta la ultima posicion
+                                                                // del ArrayList
+                                                                // con el .size() sabemos el tamaño del ArrayList
+                                                                if (recursosList.get(i).getRecursoId()
+                                                                        .equals(Id_delrecurso)) {
+                                                                    // si el ID que damos es igual a uno que este
+                                                                    // guardado en X posicion, por eso el (i), que es la
+                                                                    // posicion
+                                                                    encontradoparaelminar = true; // entonces esto se
+                                                                                                  // activa
+                                                                    recursosList.remove(i); // y elimina el recurso de
+                                                                                            // esa posicion
+                                                                    JOptionPane.showMessageDialog(null,
+                                                                            "Recurso eliminado");
+                                                                }
+                                                                        }
+                                                            }while(confirmacion != 'N' && confirmacion != 'n');
+                                                            if (!encontradoparaelminar) {
+                                                                JOptionPane.showMessageDialog(null, "ID no encontrado");
+                                                            }
+                                                            break;
+
+                                                        default:
+                                                            JOptionPane.showMessageDialog(null,
+                                                                    "ERROR, solo pueden numero del 1 al 5");
+                                                            break;
+                                                    }
+                                                } while (opcionrecursos != 5);
                                                 break;
+                                                
+                                                // ------------------------------------------------------------------------------------
                                             case 4:
                                                 // aqui iria el submenu de patrocinio
                                                 break;
