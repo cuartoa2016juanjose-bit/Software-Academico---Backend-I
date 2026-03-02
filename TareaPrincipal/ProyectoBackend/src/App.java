8fe1d4615e654dc8e869cc3c695960602f9f6000
Juan Jose Duarte Urrego <cuartoa2016juanjose@gmail.com>
2026-03-01 19:13:49 -0500

    creacion de arrays

--- TareaPrincipal/ProyectoBackend/src/App.java
+++ TareaPrincipal/ProyectoBackend/src/App.java
@@ -14,6 +14,15 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        // Creamos un ArrayList para cada modelo
+        // esto es como un invenatario para cada modelo, donde guardaremos los objetos
+        // creados de cada clase
+        ArrayList<proyectos> proyectosList = new ArrayList<>();
+        ArrayList<participantes> participantesList = new ArrayList<>();
+        ArrayList<recursos> recursosList = new ArrayList<>();
+        ArrayList<patrocinio> patrocinioList = new ArrayList<>();
+        ArrayList<mantenimientoderecursos> mantenimientoList = new ArrayList<>();
+        ArrayList<registro> registroList = new ArrayList<>();
 
     }
 
