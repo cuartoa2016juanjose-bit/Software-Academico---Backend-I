41e4a3492d21e8259f906289321a9cbf86ccba25
Juan Jose Duarte Urrego <cuartoa2016juanjose@gmail.com>
2026-03-01 19:18:49 -0500

    creacion de variables

--- TareaPrincipal/ProyectoBackend/src/App.java
+++ TareaPrincipal/ProyectoBackend/src/App.java
@@ -24,6 +24,62 @@ public class App {
         ArrayList<mantenimientoderecursos> mantenimientoList = new ArrayList<>();
         ArrayList<registro> registroList = new ArrayList<>();
 
+        // CREACION DE VARIABLES PARA PROYECTOS
+        /*
+         * creamos variables temporales para cada atributo de cada modelo.
+         * cuando hagamos el " new Xmodelo" le pasaremos los datos temporales a cada
+         * cosntructor de cada clase
+         * y despues agregaremos el objeto creado a su inventario propio, osea a los
+         * ArrayList.
+         */
+
+        // registro
+
+        String Id_delregistro = " ";
+        String nombre_delregistro;
+        String telefono_delregistro = " ";
+        String correo_delregistro;
+        String contraseña_delregistro = " ";
+
+        // mantenimiento de recursos
+        String Id_mantenimientoderecursos = " ";
+        String nombre_mantenimientoderecursos;
+        String categoria_mantenimientoderecursos;
+        String estado_mantenimientoderecursos;
+        String ubicacion_mantenimientoderecursos;
+        String fechadeingreso_mantenimientoderecursos = " ";
+
+        // parcipantes
+        String Id_delosparticipantes = " ";
+        String nombre_delosparticipantes;
+        String ubicacion_delosparticipantes;
+        String correo_delosparticipantes;
+        String rol_delosparticipante;
+
+        // patrocinio
+        String Id_delpatrocinador = " ";
+        String nombre_delpatrocinador;
+        String contacto_delpatrocinador = " ";
+        String tipo_depatrocinador;
+        String aporte_delpatrocinador = " ";
+
+        // proyectos
+        String Id_delproyecto = " ";
+        String nombre_delproyecto;
+        String tipo_delproyecto;
+        String descripcion_delproyecto;
+        String fechaInicio_delproyecto = " ";
+        String fechaFin_delproyecto = " ";
+        String estado_delproyecto;
+
+        // recursos
+        String Id_delrecurso = " ";
+        String nombre_delrecurso;
+        String categoria_delrecurso;
+        String estado_delrecurso;
+        String ubicacion_delrecurso;
+
+
     }
 
 }
