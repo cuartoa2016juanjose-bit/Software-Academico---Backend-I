ff34a4d974abf5d97ad7d5e9bbaeda2886a6f92a
Juan Jose Duarte Urrego <cuartoa2016juanjose@gmail.com>
2026-03-01 19:04:58 -0500

    primer commit

---
+++ TareaPrincipal/ProyectoBackend/src/App.java
@@ -0,0 +1,20 @@
+
+//Importamos los modelos
+import models.mantenimientoderecursos;
+import models.participantes;
+import models.patrocinio;
+import models.proyectos;
+import models.recursos;
+import models.registro;
+
+// importamos el array
+import java.util.ArrayList;
+//salida de datos, consola o ventana emergente, en este caso usaremos una ventana emergente.
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
